497b11d20a77beb7f15347a3024568f3c38562ad
David Pessanha <davidppessanha@gmail.com>
2021-10-03 19:53:42 -0300

    init vue project + installed vuetify + set home and favorites routes

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app-bar id="nav-bar" app color="purple darken-4" dark>
+    <div class="d-flex align-center">
+      <router-link to="/"><h1>Punk Beer</h1></router-link>
+    </div>
+    <v-spacer></v-spacer>
+    <router-link :to="{ name: 'favorites' }"
+      >See your favorite beers</router-link
+    >
+  </v-app-bar>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  color: white;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,27 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from "vue"
+import VueRouter from "vue-router"
+import Home from "../views/Home.vue"
+import Favorites from "../views/Favorites.vue"
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "home",
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/favorites",
+    name: "favorites",
+    component: Favorites,
   },
-];
+]
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
-});
+})
 
-export default router;
+export default router
