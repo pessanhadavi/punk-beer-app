e8d7085cc55554c811934e23a8b5e059f5b085c2
David Pessanha <davidppessanha@gmail.com>
2021-10-05 19:52:43 -0300

    Positioned favorite button component on Beer card + adjusted img width

--- src/components/Beer.vue
+++ src/components/Beer.vue
@@ -1,15 +1,25 @@
 <template>
-  <div class="beer-wrapper d-flex align-center pa-6 ma-3">
+  <div class="beer-card-wrapper d-flex align-center pa-6 ma-3">
+
+    <div class="beer-card-img">
     <img :src="beer.image_url" />
-    <div class="beer-text ml-10">
+    </div>
+
+    <div class="beer-card-text ml-10">
       <h2 class="mb-3">{{ beer.name }}</h2>
       <p>{{ beer.tagline }}</p>
     </div>
+    <FavoriteBtn class="fave-btn align-self-start" />
   </div>
 </template>
 
 <script>
+import FavoriteBtn from "@/components/FavoriteBtn.vue"
+
 export default {
+  components: {
+    FavoriteBtn,
+  },
   props: {
     beer: Object,
   },
@@ -17,23 +27,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.beer-wrapper {
+.beer-card-wrapper {
+  position: relative;
   height: 210px;
-  width: 400px;
+  width: 415px;
   background: white;
   box-shadow: 0 0 7px rgba(0, 0, 0, 0.2);
-  img {
-    height: 100%;
-  }
   &:hover {
     box-shadow: 0 0 8px rgba(255, 0, 0, 0.747);
     img {
-      height: 104%;
+      height: 105%;
     }
   }
 }
 
-.beer-text {
-  width: 400px;
+.beer-card-img {
+  height: 150px;
+  width: 70px;
+  img {
+    height: 100%;
+  }
+}
+
+.fave-btn {
+  position: absolute;
+  top: 5%;
+  right: 3%;
 }
 </style>
