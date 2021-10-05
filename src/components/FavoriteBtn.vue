1073ff45057f469b3873d17e89823114611c1b3d
David Pessanha <davidppessanha@gmail.com>
2021-10-05 19:09:10 -0300

    Created favorite button component

---
+++ src/components/FavoriteBtn.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="favorite-btn">
+    <v-btn icon color="yellow" x-small>
+      <v-icon>{{ isFave ? "mdi-star" : "mdi-star-outline" }}</v-icon>
+    </v-btn>
+  </div>
+</template>
