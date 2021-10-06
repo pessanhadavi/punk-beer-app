d92a2bb5402b6cc1e31b9a2386fe312703556b27
David Pessanha <davidppessanha@gmail.com>
2021-10-06 15:07:48 -0300

    Created Favorites views template

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    <h1>This is the favorites page</h1>
+    <h1 class="my-6">Your favorite beers:</h1>
+    <v-row>
+      <v-col cols="12" class="d-flex flex-wrap justify-center">
+        <BeerCard v-for="beer in faveBeers" :key="beer.id" :beer="beer" />
+      </v-col>
+    </v-row>
   </div>
 </template>
+
+<script>
+import BeerCard from "@/components/BeerCard.vue"
+import { mapState } from "vuex"
+
+export default {
+  components: {
+    BeerCard,
+  },
+  computed: mapState(["faveBeers"]),
+}
+</script>
